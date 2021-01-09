dd8aa9a9ddf5b23507d6cc05275066b0a97e6eab
jimao <1982775886@qq.com>
2021-01-09 15:37:12 +0800

    bip39完善中

--- src/bip39.go
+++ src/bip39.go
@@ -1,1 +1,110 @@
 package main
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"io/ioutil"
+	"strings"
+
+	"golang.org/x/crypto/pbkdf2"
+)
+
+// BIP39 bip39标准实例
+type BIP39 struct {
+	words []string
+}
+
+// Words 获取助记词切片
+func (me *BIP39) Words() []string {
+	return me.words
+}
+
+// GetIndex 根据单词获取索引
+func (me *BIP39) GetIndex(word string) int {
+	for index, item := range me.words {
+		if item == word {
+			return index
+		}
+	}
+	return -1
+}
+
+// BIP39BytesAddCheckInfo 为字节切片添加校验尾部并且生成位切片
+func BIP39BytesAddCheckInfo(bytes []byte) []int {
+	bits := BytesToBits(bytes)
+	checkLen := len(bits) / 32
+	h := sha256.New()
+	h.Write(bytes)
+	hashBytes := h.Sum(nil)
+	hashBits := BytesToBits(hashBytes)
+	bits = append(bits, hashBits[:checkLen]...)
+	return bits
+}
+
+// BIP39GetSeed 根据助记词列表和密码获取种子
+func BIP39GetSeed(words string, pwd string) []byte {
+	return pbkdf2.Key([]byte(words), []byte("mnemonic"+pwd), 2048, 64, sha512.New)
+}
+
+// GetWords 根据字节切片获取助记词切片
+func (me *BIP39) GetWords(bytes []byte) []string {
+	bits := BIP39BytesAddCheckInfo(bytes)
+	segNum := len(bits) / 11
+	modNum := len(bits) % 11
+	if modNum != 0 {
+		panic("位数不是11的整数倍")
+	}
+	rst := []string{}
+	for i := 0; i < segNum; i++ {
+		start := i * 11
+		bitSeg := bits[start : start+11]
+		num := 0
+		num += bitSeg[0] * 1024
+		num += bitSeg[1] * 512
+		num += bitSeg[2] * 256
+		num += bitSeg[3] * 128
+		num += bitSeg[4] * 64
+		num += bitSeg[5] * 32
+		num += bitSeg[6] * 16
+		num += bitSeg[7] * 8
+		num += bitSeg[8] * 4
+		num += bitSeg[9] * 2
+		num += bitSeg[10] * 1
+		rst = append(rst, me.words[num])
+	}
+	return rst
+}
+
+// GetBytes 根据助记词切片获取字节切片
+func (me *BIP39) GetBytes(words []string) []byte {
+	return nil
+}
+
+// ReadWordList 读取助记词词典列表
+func ReadWordList(filePath string) []string {
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	rst := []string{}
+	for _, item := range strings.Split(string(bytes), "\n") {
+		itemTrimed := strings.TrimSpace(item)
+		if len(itemTrimed) > 0 {
+			rst = append(rst, itemTrimed)
+		}
+	}
+	if len(rst) != 2048 {
+		panic("词典内单词数量不为2048")
+	}
+	return rst
+}
+
+// ReadWordListEn 读取英文助记词词典列表
+func ReadWordListEn() []string {
+	return ReadWordList("bip39wordlist-en.txt")
+}
+
+// ReadWordListZh 读取中文助记词词典列表
+func ReadWordListZh() []string {
+	return ReadWordList("bip39wordlist-zh.txt")
+}
