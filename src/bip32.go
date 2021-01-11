7cf309beb20e48c432e6da9074a8df99963ed14e
jimao <1982775886@qq.com>
2021-01-11 17:42:57 +0800

    实现bip32中

--- src/bip32.go
+++ src/bip32.go
@@ -1,7 +1,31 @@
 package main
 
-// BIP32 bip32标准实例
-type BIP32 struct {
-	keyBytes       []byte
-	chainCodeBytes []byte
+import (
+	"crypto/hmac"
+	"crypto/sha512"
+)
+
+// BIP32Key bip32标准的key结构
+type BIP32Key struct {
+	version     []byte
+	depth       []byte
+	fingerPrint []byte
+	childNumber []byte
+	chainCode   []byte
+	key         []byte
+	isPrivate   bool
+}
+
+// NewRootPriKey 构造函数
+func NewRootPriKey() *BIP32Key {
+	return nil
+}
+
+// BIP32GetRootPriKey 根据种子获取根私钥
+func BIP32GetRootPriKey(seed []byte) {
+	h := hmac.New(sha512.New, []byte("Bitcoin seed"))
+	h.Write(seed)
+	hrst := h.Sum(nil)
+	keyBytes := hrst[:32]
+	chainCodeBytes := hrst[32:]
 }
