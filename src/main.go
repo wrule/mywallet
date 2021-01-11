223c2e2f498bd72fd214557370f694a52b54b775
jimao <1982775886@qq.com>
2021-01-11 17:12:20 +0800

    牛逼

--- src/main.go
+++ src/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
 
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/tyler-smith/go-bip32"
 )
 
@@ -17,6 +20,20 @@ func main() {
 	fmt.Println(words)
 	seed := BIP39GetSeed(words, "")
 	fmt.Println(hex.EncodeToString(seed))
+
+	h := hmac.New(sha512.New, []byte("Bitcoin seed"))
+	h.Write(seed)
+	rst := h.Sum(nil)
+	fmt.Println(hex.EncodeToString(rst), len(rst))
+	keyBytes := rst[:32]
+	// codeBytes := rst[32:]
+
+	fmt.Println(base58.Encode(keyBytes))
+
 	computerVoiceMasterKey, _ := bip32.NewMasterKey(seed)
-	fmt.Println(computerVoiceMasterKey.B58Serialize())
+	brst, err := computerVoiceMasterKey.Serialize()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(123, base58.Encode(brst))
 }
