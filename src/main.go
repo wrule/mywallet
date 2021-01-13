775b3a8ed1f9ea33f1ac23faf8f3e3a5ea1ec57d
jimao <1982775886@qq.com>
2021-01-13 10:12:47 +0800

    删除无用注释

--- src/main.go
+++ src/main.go
@@ -27,9 +27,4 @@ func main() {
 	myRootPriKey := BIP32NewRootPriKey(seed)
 	fmt.Println(myRootPriKey.SerializeBase58())
 	fmt.Println(myRootPriKey.PublicKey().SerializeBase58())
-
-	// b32 := BIP32NewRootPriKey(seed)
-	// fmt.Println(b32.SerializeBase58())
-	// xprv9s21ZrQH143K2JF8RafpqtKiTbsbaxEeUaMnNHsm5o6wCW3z8ySyH4UxFVSfZ8n7ESu7fgir8imbZKLYVBxFPND1pniTZ81vKfd45EHKX73
-	// xprv9s21ZrQH143K3t4UZrNgeA3w861fwjYLaGwmPtQyPMmzshV2owVpfBSd2Q7YsHZ9j6i6ddYjb5PLtUdMZn8LhvuCVhGcQntq5rn7JVMqnie
 }
