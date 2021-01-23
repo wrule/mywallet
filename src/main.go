9ca16d7abeb3cae0fe7c6c2cd5f448bb6dd4a3cd
jimao <1982775886@qq.com>
2021-01-23 13:13:04 +0800

    私钥生成的私钥实验成功

--- src/main.go
+++ src/main.go
@@ -8,23 +8,29 @@ import (
 )
 
 func main() {
+	// 根据随机数生成种子
 	bytes, err := hex.DecodeString("0c1e24e5917779d297e14d45f14e1a1a")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(hex.EncodeToString(bytes))
 	bip39 := NewBIP39ByFile("/home/gu/github/mywallet/bip39wordlist-en.txt")
 	words := bip39.GenerateWords(bytes)
 	fmt.Println(words)
 	seed := BIP39GetSeed(words, "")
 	fmt.Println(hex.EncodeToString(seed))
-
+	// 生成主私钥，主公钥，以及子密钥
 	rootPriKey, _ := bip32.NewMasterKey(seed)
 	rootPubKey := rootPriKey.PublicKey()
 	fmt.Println(rootPriKey.B58Serialize())
 	fmt.Println(rootPubKey.B58Serialize())
-
+	childPriKey, err := rootPriKey.NewChildKey(15)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(childPriKey.B58Serialize())
+	// s
 	myRootPriKey := BIP32NewRootPriKey(seed)
 	fmt.Println(myRootPriKey.BIP32Base58())
-
+	myChildPriKey := myRootPriKey.ChildKey(15)
+	fmt.Println(myChildPriKey.BIP32Base58())
 }
